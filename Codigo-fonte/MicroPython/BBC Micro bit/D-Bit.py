51242bff4b2eaf84e4267efcd839755e9c89cb85
EduardoDestefani <53948252+EduardoDestefani@users.noreply.github.com>
2020-08-29 23:48:12 -0300

    Add files via upload

---
+++ Codigo-fonte/MicroPython/BBC Micro bit/D-Bit.py
@@ -0,0 +1,90 @@
+'''
+Game Name : D-Bit (a RPG dice for BBC Micro:bit)
+Author : Eduardo Destefani Stefanato (c)
+Student's Federal University of Espírito Santo
+Play on BBC Micro:bit
+
+option 1 = D2 | option 2 = D4 | option 3 = D6 | option 4 = D8 | option 5 = D10 | option 6 = D20 | option 7 = D100
+'''
+from microbit import *
+from random import randint
+from machine import reset
+
+def d2():
+    return randint(1,2)
+def d4():
+    return randint(1,4)
+def d6():
+    return randint(1,6)
+def d8():
+    return randint(1,8)
+def d10():
+    return randint(1,10)
+def d20():
+    return randint(1,20)
+def d100():
+    return randint(1,100)
+
+loop = False
+
+option = 0
+display.show(Image.DIAMOND, delay=1000, wait=True, clear=True)
+display.show(Image.ARROW_E, delay=1000, clear=True)
+
+while True :
+    if button_b.get_presses() :
+        option += 1
+        display.show(option, wait=True, clear=False)
+        if option >= 8 :
+            display.show(Image.NO, delay=1000)
+            option = 0
+    elif button_a.get_presses() :
+        display.show(Image.HAPPY, delay=1000, wait=True, loop=False, clear=True)
+        option
+        loop = True
+        break
+
+display.scroll('Shake', delay=90, wait=True)
+while loop :
+    value = d2()
+    while option == 1 :
+        if accelerometer.was_gesture('shake') :
+            display.scroll('D2={}'.format(value), delay=120)
+        elif button_a.get_presses() :
+            reset()
+    value = d4()
+    while option == 2 :
+        if accelerometer.was_gesture('shake') :
+            display.scroll('D4={}'.format(value), delay=120)
+        elif button_a.get_presses() :
+            reset()
+    value = d6()
+    while option == 3 :
+        if accelerometer.was_gesture('shake') :
+            display.scroll('D6={}'.format(value), delay=120)
+        elif button_a.get_presses() :
+            reset()
+    value = d8()
+    while option == 4 :
+        if accelerometer.was_gesture('shake') :
+            display.scroll('D8={}'.format(value), delay=120)
+        elif button_a.get_presses() :
+            reset()
+    value = d10()
+    while option == 5 :
+        if accelerometer.was_gesture('shake') :
+            display.scroll('D10={}'.format(value), delay=120)
+        elif button_a.get_presses() :
+            reset()
+    value = d20()
+    while option == 6 :
+        if accelerometer.was_gesture('shake') :
+            display.scroll('D20={}'.format(value), delay=120)
+        elif button_a.get_presses() :
+            reset()
+    value = d100()
+    while option == 7 :
+        if accelerometer.was_gesture('shake') :
+            display.scroll('D100={}'.format(value), delay=120)
+        elif button_a.get_presses() :
+            reset()
---
+++ Codigo-fonte/MicroPython/BBC Micro bit/README.md
@@ -0,0 +1,26 @@
+# BBC Micro:bit MicroPython practices
+
+Public repository for practices and postings of micropython codes for BBC Micro: bit boards.
+
+## Codes
+Summary of codes :
+
+#### 1. Game codes
+
+1.1. [Le-ght]() (a left and right game) ; it is a game based on micropython that consists of reflexes. 
+
+- Operation :
+  - The player must choose the difficulty (level 1 to 4, are alternated by pressing button B), and prepare for the instructions that will appear on the BBC Micro's 25 LED screen: bit;
+  -  To confirm the option of difficulty, press button A and wait for the instructions on the screen;
+  - The instructions are; left or right, displayed through an arrow that points to button B (left) or button A (right);
+  - The player must press the button that refers to the indicated direction;
+  - With each hit, the arrow is less displayed on the screen, if the player misses, the game restarts and shows the amount of points reached.
+
+1.2. [D-Bit]() (a RPG dice for BBC Micro:bit) ; it is a micropython software that simulates RPG dice.
+
+- Operation :
+  - The player must select the type of dice, being them; D2, D4, D6, D8, D10, D20 and D100;
+  - To select, he must choose the option for each data, for example (D2 = option 1, D4 = option 2, ... D100 = option 7);
+  - The options (from 1 to 7, D2 to D100), are alternated by pressing button B. To confirm the option, press button A and wait for the "Shake" instruction on the screen;
+  - After the instruction displayed on the screen, swing the BBC Micro: bit and wait for the result;
+  - If you want to play again, press button A to restart the software.
